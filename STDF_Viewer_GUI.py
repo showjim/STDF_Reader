b821a292925323c8f07983d64a589af66320d326
Chao Zhou <zhouchao486@gmail.com>
2020-08-05 17:18:58 +0800

    add GUI

--- STDF_Viewer_GUI.py
+++ STDF_Viewer_GUI.py
@@ -1,51 +1,107 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from stdf.stdf_reader import Reader
+from stdf.stdf_writer import Writer
+import logging
+from pathlib import Path
+import time
+
+class Table(QWidget):
+    def __init__(self, parent=None):
+        super(Table, self).__init__(parent)
+        # 设置标题与初始大小
+        self.setWindowTitle('STDF Viewer Beta V0.1')
+        self.resize(500, 300)
+
+        row_num = len(stdf_dic)
+        col_num = 4
+        # 设置数据层次结构，4行4列
+        self.model = QStandardItemModel(row_num, col_num)
+        # 设置水平方向四个头标签文本内容
+        self.model.setHorizontalHeaderLabels(['Index', 'Records', 'Count','Position'])
+
+        # #Todo 优化2 添加数据
+        # self.model.appendRow([
+        #   QStandardItem('row %s,column %s' % (11,11)),
+        #   QStandardItem('row %s,column %s' % (11,11)),
+        #   QStandardItem('row %s,column %s' % (11,11)),
+        #   QStandardItem('row %s,column %s' % (11,11)),
+        # ])
+        i=0
+        j = 0
+        last_rec = ''
+        for key, val in stdf_dic.items():
+            index_rec_list = key.split(' - ')
+            index = index_rec_list[0]
+            rec = index_rec_list[1]
+
+            if rec == last_rec:
+                j += 1
+            else:
+                j = 0
+                index_item = QStandardItem(index)
+                rec_item = QStandardItem(rec)
+                pos_item = QStandardItem(val)
+                cnt_item = QStandardItem(j)
+                self.model.setItem(int(index), 0, index_item)
+                self.model.setItem(int(index), 1, rec_item)
+                self.model.setItem(int(index), 2, cnt_item)
+                self.model.setItem(int(index), 3, pos_item)
+            last_rec = rec
+            # i +=1
+            # if i==1000:
+            #     break
+        # for row in range(row_num):
+        #     for column in range(col_num):
+        #         temp_str =
+        #         item = QStandardItem(temp_str) #QStandardItem('row %s,column %s' % (row, column))
+        #         # 设置每个位置的文本值
+        #         self.model.setItem(row, column, item)
+
+        # 实例化表格视图，设置模型为自定义的模型
+        self.tableView = QTableView()
+        self.tableView.setModel(self.model)
+
+        # #todo 优化1 表格填满窗口
+        # #水平方向标签拓展剩下的窗口部分，填满表格
+        # self.tableView.horizontalHeader().setStretchLastSection(True)
+        # #水平方向，表格大小拓展到适当的尺寸
+        # self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        #
+        # #TODO 优化3 删除当前选中的数据
+        # indexs=self.tableView.selectionModel().selection().indexes()
+        # print(indexs)
+        # if len(indexs)>0:
+        #   index=indexs[0]
+        #   self.model.removeRows(index.row(),1)
+
+        # 设置布局
+        layout = QVBoxLayout()
+        layout.addWidget(self.tableView)
+        self.setLayout(layout)
 
 
-class TableModel(QtCore.QAbstractTableModel):
-    def __init__(self, data):
-        super(TableModel, self).__init__()
-        self._data = data
+def get_all_records(stdf):
+    stdf.read_rec_list = True
+    stdf_dic = {}
+    i = 0
+    for rec_name, position in stdf:
+        stdf_dic[str(i) + ' - ' + rec_name] = position
+        i += 1
+    stdf.read_rec_list = False
+    return stdf_dic
 
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
-
-    def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
-
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    in_file = r'./sample_stdf/a595.stdf'
+    stdf = Reader()
+    stdf.load_stdf_file(stdf_file=in_file)
+    stdf_dic = get_all_records(stdf)
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.table = QtWidgets.QTableView()
-
-        data = [
-          [4, 9, 2],
-          [1, 0, 0],
-          [3, 5, 0],
-          [3, 3, 2],
-          [7, 8, 9],
-        ]
-
-        self.model = TableModel(data)
-        self.table.setModel(self.model)
-
-        self.setCentralWidget(self.table)
-
-
-app=QtWidgets.QApplication(sys.argv)
-window=MainWindow()
-window.show()
-app.exec_()
+    app = QApplication(sys.argv)
+    table = Table()
+    table.show()
+    sys.exit(app.exec_())
