dcce78ce58786c027d4bccd4d2c160e6092f5202
Jerry <zhouchao486@gmail.com>
2020-08-26 12:45:27 +0800

    Update stdf_reader.py

--- stdf/stdf_reader.py
+++ stdf/stdf_reader.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """The MIT License (MIT)
 Copyright (c) 2016 Cahyo Primawidodo
 
@@ -15,21 +16,19 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE."""
 
-import json
 import io
 import struct
 import logging
 import re
 import math
-from os import path
-
-__author__ = 'cahyo primawidodo 2016'
+import gzip
+from stdf.stdf_type_V4_2007_1 import TYPE
 
 
 class Reader:
     HEADER_SIZE = 4
 
-    def __init__(self, stdf_ver_json=None):
+    def __init__(self):
         self.log = logging.getLogger(self.__class__.__name__)
         self.STDF_TYPE = {}
         self.STDF_IO = io.BytesIO(b'')
@@ -40,21 +39,21 @@ class Reader:
         self.body_start = 0
 
         self._load_byte_fmt_mapping()
-        self._load_stdf_type(json_file=stdf_ver_json)
+        self._load_stdf_type()
 
         self.read_rec_list = False
 
-    def _load_stdf_type(self, json_file):
+    def _load_stdf_type(self):
 
-        if json_file is None:
-            here = path.abspath(path.dirname(__file__))
-            input_file = path.join(here, 'stdf_v4.json')
-        else:
-            input_file = json_file
-
-        self.log.info('loading STDF configuration file = {}'.format(input_file))
-        with open(input_file) as fp:
-            self.STDF_TYPE = json.load(fp)
+        # if json_file is None:
+        #     here = path.abspath(path.dirname(__file__))
+        #     input_file = path.join(here, 'stdf_v4.json')
+        # else:
+        #     input_file = json_file
+        #
+        # self.log.info('loading STDF configuration file = {}'.format(input_file))
+        # with open(input_file) as fp:
+        self.STDF_TYPE = TYPE # json.load(fp)
 
         for k, v in self.STDF_TYPE.items():
             typ_sub = (v['rec_typ'], v['rec_sub'])
@@ -79,8 +78,13 @@ class Reader:
 
     def load_stdf_file(self, stdf_file):
         self.log.info('opening STDF file = {}'.format(stdf_file))
-        with open(stdf_file, mode='rb') as fs:
-            self.STDF_IO = io.BytesIO(fs.read())
+        if stdf_file.endswith(".std") or stdf_file.endswith(".stdf"):
+            with open(stdf_file, mode='rb') as fs:
+                self.STDF_IO = io.BytesIO(fs.read())
+        elif stdf_file.endswith(".gz"):
+            with gzip.open(stdf_file, mode='rb') as fs:
+                self.STDF_IO = io.BytesIO(fs.read())
+
         self.log.info('detecting STDF file size = {}'.format(len(self.STDF_IO.getvalue())))
 
     def auto_detect_endian(self):
@@ -112,7 +116,14 @@ class Reader:
             body_raw = self._read_body(rec_size)
             rec_name, body = self._unpack_body(header, body_raw)
             self.log.debug('BODY end at tell={:0>8}'.format(self.STDF_IO.tell()))
-
+            # To show all the fields
+            if len(body) < len(self.STDF_TYPE[rec_name]['body']):
+                for field, val in self.STDF_TYPE[rec_name]['body']:
+                    if field in body:
+                        pass
+                    else:
+                        body[field] = 'N/A'
+                pass
             # if rec_name == 'FAR':
             #     self.__set_endian(body['CPU_TYPE'])
 
@@ -138,7 +149,7 @@ class Reader:
     def _read_body(self, rec_size):
         self.body_start = self.STDF_IO.tell()
         body_raw = io.BytesIO(self.STDF_IO.read(rec_size))
-        assert len(body_raw.getvalue()) == rec_size
+        # assert len(body_raw.getvalue()) == rec_size
         return body_raw
 
     def _unpack_body(self, header, body_raw):
@@ -161,7 +172,7 @@ class Reader:
                 array_data = []
                 if fmt_raw.startswith('K'):
                     mo = re.match('^K([0xn])(\w{2})', fmt_raw)
-                    n = self.__get_multiplier(field, body)
+                    n = self.__get_multiplier(rec_name, field, body)
                     fmt_act = mo.group(2)
 
                     for i in range(n):
@@ -198,7 +209,7 @@ class Reader:
         return rec_name, body
 
     def __get_data(self, fmt_act, body_raw, odd_nibble):
-        data = 0
+        data = '' #0
         if fmt_act == 'N1':
             if odd_nibble:
                 nibble, = struct.unpack(self.e + 'B', body_raw.read(1))
@@ -270,48 +281,76 @@ class Reader:
             raise IOError(cpu_type)
 
     @staticmethod
-    def __get_multiplier(field, body):
-        if field == 'SITE_NUM':
-            return body['SITE_CNT']  # SDR (1, 80)
+    def __get_multiplier(rec_name, field, body):
 
-        elif field == 'PMR_INDX':
-            return body['INDX_CNT']  # PGR (1, 62)
+        if rec_name == 'SDR':
+            if field == 'SITE_NUM':
+                return body['SITE_CNT']  # SDR (1, 80)
+        if rec_name == 'PGR':
+            if field == 'PMR_INDX':
+                return body['INDX_CNT']  # PGR (1, 62)
+        if rec_name == 'PLR':
+            if field in ['GRP_INDX', 'GRP_MODE', 'GRP_RADX', 'PGM_CHAR', 'RTN_CHAR', 'PGM_CHAL', 'RTN_CHAL']:
+                return body['GRP_CNT']  # PLR (1, 63)
+        if rec_name == 'FTR':
+            if field in ['RTN_INDX', 'RTN_STAT']:
+                return body['RTN_ICNT']  # FTR (15, 20)
+            elif field in ['PGM_INDX', 'PGM_STAT']:
+                return body['PGM_ICNT']  # FTR (15, 20)
+        if rec_name == 'MPR':
+            if field in ['RTN_STAT', 'RTN_INDX']:
+                return body['RTN_ICNT']  # MPR (15, 15)
 
-        elif field in ['GRP_INDX', 'GRP_MODE', 'GRP_RADX', 'PGM_CHAR', 'RTN_CHAR', 'PGM_CHAL', 'RTN_CHAL']:
-            return body['GRP_CNT']  # PLR (1, 63)
-
-        elif field in ['RTN_INDX', 'RTN_STAT']:
-            return body['RTN_ICNT']  # FTR (15, 20)
-
-        elif field in ['PGM_INDX', 'PGM_STAT']:
-            return body['PGM_ICNT']  # FTR (15, 20)
-
-        elif field in ['RTN_STAT', 'RTN_INDX']:
-            return body['RTN_ICNT']  # MPR (15, 15)
-
-        elif field in ['RTN_RSLT']:
-            return body['RSLT_CNT']  # MPR (15, 15)
-
-        elif field in ['UPD_NAM']:
-            return body['UPD_CNT']  # VUR (0, 30)
-
-        elif field in ["PAT_BGN", "PAT_END", "PAT_FILE", "PAT_FILE", "PAT_LBL", "FILE_UID", "ATPG_DSC", "SRC_ID"]:
-            return body["LOCP_CNT"]  # PSR (1, 90)
-
-        elif field in ["PMR_INDX", "ATPG_NAM"]:
-            return body["LOCM_CNT"]  # NMR (1, 91)
-
-        elif field in ["CHN_LIST"]:
-            return body["CHN_CNT"]  # SSR (1, 93)
-
-        elif field in ["M_CLKS"]:
-            return body["MSTR_CNT"]  # CDR (1, 94)
-
-        elif field in ["S_CLKS"]:
-            return body["SLAV_CNT"]  # CDR (1, 94)
-
-        elif field in ["CELL_LST"]:
-            return body["LST_CNT"]  # CDR (1, 94)
+            elif field in ['RTN_RSLT']:
+                return body['RSLT_CNT']  # MPR (15, 15)
+        if rec_name == 'VUR':
+            if field in ['UPD_NAM']:
+                return body['UPD_CNT']  # VUR (0, 30)
+        if rec_name == 'PSR':
+            if field in ["PAT_BGN", "PAT_END", "PAT_FILE", "PAT_LBL", "FILE_UID", "ATPG_DSC", "SRC_ID"]:
+                return body["LOCP_CNT"]  # PSR (1, 90)
+        if rec_name == 'NMR':
+            if field in ["PMR_INDX", "ATPG_NAM"]:
+                return body["LOCM_CNT"]  # NMR (1, 91)
+        if rec_name == 'SSR':
+            if field in ["CHN_LIST"]:
+                return body["CHN_CNT"]  # SSR (1, 93)
+        if rec_name == 'CDR':
+            if field in ["M_CLKS"]:
+                return body["MSTR_CNT"]  # CDR (1, 94)
+            elif field in ["S_CLKS"]:
+                return body["SLAV_CNT"]  # CDR (1, 94)
+            elif field in ["CELL_LST"]:
+                return body["LST_CNT"]  # CDR (1, 94)
+        if rec_name == 'STR':
+            if field in ['LIM_INDX', 'LIM_SPEC']:
+                return body["LIM_CNT"]  # STR
+            elif field in ['COND_LST']:
+                return body["COND_CNT"]  # STR
+            elif field in ['CYC_OFST']:
+                return body["CYCO_CNT"]  # STR
+            elif field in ['PMR_INDX']:
+                return body["PMR_CNT"]  # STR
+            elif field in ['CHN_NUM']:
+                return body["CHN_CNT"]  # STR
+            elif field in ['EXP_DATA']:
+                return body["EXP_CNT"]  # STR
+            elif field in ['CAP_DATA']:
+                return body["CAP_CNT"]  # STR
+            elif field in ['NEW_DATA']:
+                return body["NEW_CNT"]  # STR
+            elif field in ['PAT_NUM']:
+                return body["PAT_CNT"]  # STR
+            elif field in ['BIT_POS']:
+                return body["BPOS_CNT"]  # STR
+            elif field in ['USR1']:
+                return body["USR1_CNT"]  # STR
+            elif field in ['USR2']:
+                return body["USR2_CNT"]  # STR
+            elif field in ['USR3']:
+                return body["USR3_CNT"]  # STR
+            elif field in ['USER_TXT']:
+                return body["TXT_CNT"]  # STR
 
         else:
             raise ValueError
